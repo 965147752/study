4d31ad6603c67fd14c0dc7437312ea5bc2fe258d
dengzb <965147752@qq.com>
2018-02-05 16:57:06 +0800

    版本1.0.0

---
+++ src/main/java/cn/dengzb/Application.java
@@ -0,0 +1,13 @@
+package cn.dengzb;
+
+/**
+ * @Author: dengzb
+ * @Description:
+ * @Date: 2018/2/5
+ */
+public class Application {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(DengzbApplication.class, args);
+    }
+}
